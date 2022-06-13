9539b0b4739e8faff125569ad0701220eadca6a0
lishaobo-baron <s.li@reply.com>
2022-06-13 10:27:21 +0800

    initial

---
+++ sources/Cosmos.Core/LogHelper.cs
@@ -0,0 +1,34 @@
+﻿namespace Cosmos.Core;
+
+public class LogHelper
+{
+    public static void WriteLog(string str, string category = "ping_log")
+    {
+        string sFilePath = $"C:\\{category}\\" + DateTime.Now.ToString("yyyy-MM-dd");
+        //string sFilePath = filepath+ _sFilePath;
+        string sFileName = "DSM" + "-" + DateTime.Now.ToString("yyyy-MM-dd hh") + ".log";
+        sFileName = sFilePath + "\\" + sFileName; //the absolute path of file 
+        if (!Directory.Exists(sFilePath))//Verify that the path exists
+        {
+            Directory.CreateDirectory(sFilePath);
+            //Create if not present
+        }
+        FileStream fs;
+        StreamWriter sw;
+        if (File.Exists(sFileName))
+        //verify that the file exists ,append if present ,creat if not present
+        {
+            fs = new FileStream(sFileName, FileMode.Append, FileAccess.Write);
+        }
+        else
+        {
+            fs = new FileStream(sFileName, FileMode.Create, FileAccess.Write);
+        }
+        sw = new StreamWriter(fs);
+        sw.Write(str);
+        sw.Close();
+        fs.Close();
+    }
+
+}
+
---
+++ sources/Cosmos.Core/PingHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmos.Core;
+
+public static class PingHelper
+{
+    public static async Task<bool> PingIPAsync(string host,CancellationToken token)
+    {
+        try
+        {
+            var ping = new System.Net.NetworkInformation.Ping();
+            var reply = await ping.SendPingAsync(host).WaitAsync(token);
+            if (reply.Status == System.Net.NetworkInformation.IPStatus.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public static async Task PingPortAsync(string host,string port, CancellationToken token)
+    {
+        try
+        {
+            System.Net.IPAddress iPAddress = System.Net.IPAddress.Parse(host);
+            System.Net.IPEndPoint ep = new System.Net.IPEndPoint(iPAddress, Convert.ToInt32(port));
+            using (var socket=new System.Net.Sockets.Socket(System.Net.Sockets.AddressFamily.InterNetwork,System.Net.Sockets.SocketType.Stream,System.Net.Sockets.ProtocolType.Tcp))
+            {
+                await socket.ConnectAsync(ep,token);
+            }
+        }
+        catch 
+        {
+
+        }
+    }
+}
+
